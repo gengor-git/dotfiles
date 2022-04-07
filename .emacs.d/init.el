ce9c148dc925806e726685fa817e5af4432c1ba2
gengor <martin.palmowski@gmail.com>
2022-04-07 08:25:03 +0200

    Run config automatically.

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "Emacs.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(powershell visual-fill-column ox-pandoc org-bullets org-make-toc plantuml-mode elpy python-mode company-box company dap-mode lsp-ui lsp-pyright lsp-mode yasnippet-snippets yasnippet git-gutter magit counsel-projectile projectile general undo-tree evil-surround evil-collection evil evil-nerd-commenter indent-tools rainbow-mode rainbow-delimiters helpful counsel ivy-rich ivy which-key doom-modeline all-the-icons doom-themes command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
